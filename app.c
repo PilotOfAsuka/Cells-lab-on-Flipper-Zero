2a864267892aa20261d7689bfa823b09a9d990f6
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-11-04 22:24:21 +0500

    dev_1
    Добавлены изменения температуры и времени.
    Добавлены отображение времени и температуры.
    добавлен простой мозг для клетки.

--- app.c
+++ app.c
@@ -1,24 +1,42 @@
+
 #include <stdio.h>
 #include <furi.h>
 #include <gui/gui.h>
 #include <furi_hal.h>
 #include <stdlib.h>
 #include "utils.h"
-
-#define BACKLIGHT_ON 1
+#include <gui/view.h>
 
 // Определение структуры клетки
 typedef struct {
     int32_t x, y; // Позиция клетки
     int32_t energy; // Уровень энергии
-    int32_t dna;
+    int32_t dna; // Днк клетки
+    int32_t command; // Команда клетки (что у нее на уме так сказать)
 } Cell;
 
-// Глобальные переменные
-int32_t max_cells = 2000;
-Cell cells[2000]; // Начальный массив на 1000 клеток
-int32_t cell_count = 15; // Начальное количество клеток
+// Определение структуры погоды
+typedef struct {
+    int32_t temp; // Темп.
+    int32_t temp_delay; // Значение для изменения температуры каждые N цикла
+    int32_t direction_t;
+    int32_t hour; // Время суток
+    int32_t hour_delay; // Значение для изменения времени каждые N цикла
+} Weather;
 
+// Глобальные переменные
+int32_t max_cells = 2000; // Максимальное кольво клеток
+Cell cells[2000]; // Начальный массив на 1000 клеток
+int32_t cell_count = 30; // Начальное количество клеток
+Weather weather = (Weather){
+    .hour = 0,
+    .direction_t = 1,
+    .temp = 0,
+    .temp_delay = 500,
+    .hour_delay = 1000}; // Инициализация погоды и времени
+int32_t cycle_count = 0;
+
+// Функция проверки свободной позиции
 bool is_position_occupied(int32_t x, int32_t y) {
     for(int32_t i = 0; i < cell_count; i++) {
         if(cells[i].x == x && cells[i].y == y) {
@@ -28,7 +46,8 @@ bool is_position_occupied(int32_t x, int32_t y) {
     return false; // Позиция свободна
 }
 
-void move_cell(int32_t i) {
+// Функция движения клетки
+static void move_cell(int32_t i) {
     int32_t move_x = ((cells[i].dna) % 3) - 1; // -1, 0 или 1
     int32_t move_y = ((cells[i].dna) % 3) - 1; // -1, 0 или 1
 
@@ -37,8 +56,8 @@ void move_cell(int32_t i) {
     int32_t new_y = cells[i].y + move_y;
 
     // Ограничиваем координаты в пределах сетки
-    new_x = (new_x < 0) ? 0 : (new_x > 63) ? 63 : new_x; // 32 - 1
-    new_y = (new_y < 0) ? 0 : (new_y > 63) ? 63 : new_y; // 16 - 1
+    new_x = (new_x < 0) ? 0 : (new_x > 63) ? 63 : new_x; //
+    new_y = (new_y < 0) ? 0 : (new_y > 63) ? 63 : new_y; //
 
     // Проверка на занятость новой позиции
     if(!is_position_occupied(new_x, new_y)) {
@@ -48,74 +67,174 @@ void move_cell(int32_t i) {
     }
 }
 
-// Основная логика
-static void logic_game() {
-    for(int32_t i = 0; i < cell_count; i++) {
-        // Проверка на границы квадрата и уменьшение энергии
-        if(cells[i].x < 0 || cells[i].x >= 64 || cells[i].y < 0 || cells[i].y >= 64) {
-            cells[i].energy =
-                0; // Если клетка выходит за границы, она теряет энергию (можно задать другие правила)
+// Функция фотосинтеза
+static void photosyntes(int32_t hour, int32_t i) {
+    cells[i].energy += (20 - cells[i].y + hour) % 10;
+}
+
+// Простая функция изменения времени
+static void set_hour() {
+    weather.hour = (weather.hour + 1) % 25;
+}
+
+// Простая функция темпeратуры
+static void set_temp() {
+    weather.temp += weather.direction_t;
+    // Изменение направления темпeратуры
+    if(weather.temp == 10) {
+        weather.direction_t = -1;
+    } else if(weather.temp == -10) {
+        weather.direction_t = 1;
+    }
+}
+
+// Функция установки погоды
+static void set_weather() {
+    // Обновляем погоду каждый weather.hour_delay
+    if(cycle_count % weather.hour_delay == 0) {
+        set_hour(); // Установка времени
+    }
+
+    // Обновляем темп. каждый weather.temp_delay
+    if(cycle_count % weather.temp_delay == 0) {
+        set_temp(); // Установка темп.
+    }
+    // Сброс счетчика
+    if(cycle_count >= 2000) {
+        cycle_count = 0;
+    }
+}
+
+// Функция выполнения действия клетки на основе её "ума" и генетического кода
+static void perform_action(int32_t i) {
+    // Сбрасываем команду, если она превышает максимальное число действий
+    const int32_t max_commands = 10;
+    if(cells[i].command >= max_commands) {
+        cells[i].command = 0;
+    }
+
+    // Определяем действие на основе команды и ДНК клетки
+    int32_t action = (cells[i].command + cells[i].dna) % max_commands;
+
+    // Выполняем действие на основе вычисленного значения
+    switch(action) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        photosyntes(weather.hour, i); // Фотосинтез
+        break;
+    case 5:
+    case 6:
+    case 7:
+        move_cell(i); // Движение
+        break;
+    case 8:
+    case 9:
+    }
+
+    // Увеличение значения команды клетки для следующего действия
+    cells[i].command++;
+}
+
+// Функция отнятия энергии
+static void subtract_energy(int32_t energy, int32_t i) {
+    cells[i].energy -= energy;
+}
+
+static void draw_gui(Canvas* canvas) {
+    // Устанавливаем шрифт для отображения количества клеток
+    canvas_set_font(canvas, FontSecondary);
+
+    // Создаем буферы для хранения строк
+    char buffer_cells[20];
+    char buffer_temp[20];
+    char buffer_hour[20];
+
+    snprintf(buffer_hour, sizeof(buffer_hour), "Hour: %ld", weather.hour);
+    snprintf(buffer_temp, sizeof(buffer_temp), "Temp: %ld", weather.temp);
+    snprintf(buffer_cells, sizeof(buffer_cells), "Cells: %ld", cell_count);
+
+    // Рисуем строку на экране
+    canvas_draw_str(canvas, 80, 8, buffer_cells);
+    canvas_draw_str(canvas, 80, 17, buffer_temp);
+    canvas_draw_str(canvas, 80, 26, buffer_hour);
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 110, 61, "exit");
+}
+
+static void check_dead(int32_t i) {
+    // Проверка на смерть клетки
+    if(cells[i].energy <= 0) {
+        // Удаление клетки
+        for(int j = i; j < cell_count - 1; j++) {
+            cells[j] = cells[j + 1]; // Сдвиг всех клеток влево
         }
-        cells[i].energy -= 1;
-        // Проверка на смерть клетки
-        if(cells[i].energy <= 0) {
-            // Удаление клетки
-            for(int j = i; j < cell_count - 1; j++) {
-                cells[j] = cells[j + 1]; // Сдвиг всех клеток влево
-            }
-            cell_count--; // Уменьшаем количество клеток
-            i--; // Возвращаемся назад, чтобы проверить новую клетку на этой позиции
-            continue; // Переходим к следующей итерации цикла
+        cell_count--; // Уменьшаем количество клеток
+        i--; // Возвращаемся назад, чтобы проверить новую клетку на этой позиции
+    }
+}
+
+static void cell_division(int32_t i) {
+    // Если энергии достаточно, клетка делится
+    if(cells[i].energy > 100 && cell_count < max_cells) {
+        int32_t new_x = cells[i].x + (rand() % 3 - 1);
+        int32_t new_y = cells[i].y + (rand() % 3 - 1);
+        int32_t new_dna;
+
+        // Ограничиваем координаты в пределах сетки
+        new_x = (new_x < 0) ? 0 : (new_x > 63) ? 63 : new_x; //
+        new_y = (new_y < 0) ? 0 : (new_y > 63) ? 63 : new_y; //
+
+        if(perform_action_with_chance(10)) {
+            new_dna = (cells[i].dna + rand()) % 64;
+        } else {
+            new_dna = cells[i].dna;
         }
 
-        // Фотосинтез: добавляем энергию в зависимости от зоны
-        cells[i].energy += (64 - cells[i].y) % 10;
-        move_cell(i);
-
-        // Если энергии достаточно, клетка делится
-        if(cells[i].energy > 100 && cell_count < max_cells) {
-            int32_t new_x = cells[i].x + (rand() % 3 - 1);
-            int32_t new_y = cells[i].y + (rand() % 3 - 1);
-            int32_t new_dna;
-
-            if(perform_action_with_chance(10)) {
-                new_dna = (cells[i].dna + rand()) % 64;
-            } else {
-                new_dna = cells[i].dna;
-            }
-
-            // Проверка на занятость позиции
-            if(!is_position_occupied(new_x, new_y)) {
-                cells[cell_count++] =
-                    (Cell){.x = new_x, .y = new_y, .energy = cells[i].energy / 2, .dna = new_dna};
-                cells[i].energy /= 2;
-            } else {
-                // Если позиция занята, клетка умирает
-                cells[i].energy = 0;
-            }
+        // Проверка на занятость позиции
+        if(!is_position_occupied(new_x, new_y)) {
+            cells[cell_count++] =
+                (Cell){.x = new_x, .y = new_y, .energy = cells[i].energy / 2, .dna = new_dna};
+            cells[i].energy /= 2;
+        } else {
+            // Если позиция занята, клетка умирает
+            cells[i].energy = 0;
         }
     }
 }
 
-// Функция отрисовки на экране
+// Основная логика
+static void logic_game() {
+    set_weather(); // Функция установки погоды
+
+    // Цикл проходящи по всем клеткам
+    for(int32_t i = 0; i < cell_count; i++) {
+        subtract_energy(1, i); // Отнемаем энергию у клетки за каждый цикл
+
+        check_dead(i); //Проверка на смерть клетки
+
+        perform_action(i); // Обработка действия клеткой
+
+        cell_division(i); // Проверка готовности деления клетки
+    }
+}
+
+// Функция обновления дисплея
 static void draw_callback(Canvas* canvas, void* ctx) {
     UNUSED(ctx);
-    canvas_clear(canvas);
+
     logic_game();
+
+    canvas_clear(canvas);
+
+    draw_gui(canvas);
 
     for(int32_t i = 0; i < cell_count; i++) {
         // Рисуем клетку на экране
         canvas_draw_dot(canvas, cells[i].x, cells[i].y);
     }
-    // Устанавливаем шрифт для отображения количества клеток
-    canvas_set_font(canvas, FontSecondary);
-
-    // Создаем буфер для хранения строки с количеством клеток
-    char buffer[20];
-    snprintf(buffer, sizeof(buffer), "Cells: %ld", cell_count);
-
-    // Рисуем строку на экране
-    canvas_draw_str(canvas, 80, 8, buffer);
 }
 
 // Функция обработки ввода для выхода из эффекта
@@ -166,9 +285,6 @@ int32_t cells_app_main(void* p) {
         if(event.key == InputKeyBack) {
             break;
         }
-
-        // Пауза для создания плавности эффекта
-        furi_delay_ms(1000);
     }
 
     // Очистка ресурсов
